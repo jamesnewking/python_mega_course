eea2f06b352e4e43e9752aad2f3269a208a549de
jamesnewking <jamesnewking@gmail.com>
2023-10-07 22:34:10 -0700

    context manager

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ while cycle:
     usrInput = usrInput.strip().title()
     match usrInput:
         case "Show":
-            todoFile = open('todos.txt', 'r')
-            currentList = todoFile.readlines()
-            todoFile.close()
-            for index, item in enumerate(currentList):
+            # context manager
+            with open('todos.txt', 'r') as todoFile:
+                currentList = todoFile.readlines()
+            # todoFile = open('todos.txt', 'r')
+            # currentList = todoFile.readlines()
+            # todoFile.close()
+
+            # for index, item in enumerate(currentList):
+            #     stripExtraReturn = item.strip('\n')
+            #     print(f"{index + 1}) {stripExtraReturn}")
+            filteredList = [item.strip("\n") for item in currentList]
+            # list comprehension
+            for index, item in enumerate(filteredList):
                 print(f"{index + 1}) {item}")
             if len(currentList) <= 0:
                 print("Empty list")
@@ -25,32 +34,36 @@ while cycle:
             todoFile.writelines(currentList)
             todoFile.close()
         case "Edit":
-            todoFile = open('todos.txt', 'r')
-            currentList = todoFile.readlines()
+            # todoFile = open('todos.txt', 'r')
+            with open('todos.txt', 'r') as todoFile:
+                currentList = todoFile.readlines()
             # todoFile.close()
             editIndex = int(input("Edit index number: ")) - 1
             newItem = input("New item name: ") + "\n"
             currentList[editIndex] = newItem
-            todoFile = open('todos.txt', 'w')
-            todoFile.writelines(currentList)
-            todoFile.close()
+            # todoFile = open('todos.txt', 'w')
+            with open('todos.txt', 'w') as todoFile:
+                todoFile.writelines(currentList)
+            # todoFile.close()
         case "Complete":
-            todoFile = open('todos.txt', 'r')
-            currentList = todoFile.readlines()
-            todoFile.close()
+            # todoFile = open('todos.txt', 'r')
+            with open('todos.txt', 'r') as todoFile:
+                currentList = todoFile.readlines()
+            # todoFile.close()
             removeItem = input("Which index to remove?(number or item name)")
-            if(str.isdigit(removeItem)):
+            if str.isdigit(removeItem):
                 removeItem = int(removeItem)
-                if(removeItem > len(currentList) or removeItem < 1):
+                if removeItem > len(currentList) or removeItem < 1:
                     print("Index is out of bounds. Index is between 1 to", len(currentList))
                 else:
                     removeIndex = removeItem - 1
                     print("Removed:", currentList.pop(removeIndex))
             else:
                 currentList.remove(removeItem)
-            todoFile = open('todos.txt', 'w')
-            todoFile.writelines(currentList)
-            todoFile.close()
+            # todoFile = open('todos.txt', 'w')
+            with open('todos.txt', 'w') as todoFile:
+                todoFile.writelines(currentList)
+            # todoFile.close()
         case "Exit":
             cycle = False
 print("Bye!")
