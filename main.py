ea59bdbbe8e865f00f8bccbee87dcb8811ed08f3
jamesnewking <jamesnewking@gmail.com>
2023-10-19 21:30:41 -0700

    function default arguments

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ prompt = "Your options are: Show, Add, Edit, Complete, Exit: "
 cycle = True
 
 
-def get_current_list(filepath):
+def get_current_list(filepath='todos.txt'):
+    """ Get list from text file"""
     with open(filepath, 'r') as todo_file:
         current_list = todo_file.readlines()
     return current_list
 
 
-def write_current_list(filepath, todo_list):
+def write_current_list(todo_list, filepath='todos.txt'):
+    """ Write list to text file"""
     with open(filepath, 'w') as todoFile:
         todoFile.writelines(todo_list)
 
@@ -19,7 +21,7 @@ while cycle:
     usrInput = usrInput.strip().title()
     match usrInput:
         case "Show":
-            currentList = get_current_list('todos.txt')
+            currentList = get_current_list()
             filteredList = [item.strip("\n") for item in currentList]
             # list comprehension
             for index, item in enumerate(filteredList):
@@ -28,22 +30,22 @@ while cycle:
                 print("Empty list")
         case "Add":
             addItem = input("Add this to list: ") + "\n"
-            currentList = get_current_list('todos.txt')
+            currentList = get_current_list()
             currentList.append(addItem)
-            write_current_list('todos.txt', currentList)
+            write_current_list(currentList)
             # todoFile = open('todos.txt', 'w')
             # todoFile.writelines(currentList)
             # todoFile.close()
         case "Edit":
-            currentList = get_current_list('todos.txt')
+            currentList = get_current_list()
             editIndex = int(input("Edit index number: ")) - 1
             newItem = input("New item name: ") + "\n"
             currentList[editIndex] = newItem
-            write_current_list('todos.txt', currentList)
+            write_current_list(currentList)
             # with open('todos.txt', 'w') as todoFile:
             #     todoFile.writelines(currentList)
         case "Complete":
-            currentList = get_current_list('todos.txt')
+            currentList = get_current_list()
             removeItem = input("Which index to remove?(number or item name)")
             if str.isdigit(removeItem):
                 removeItem = int(removeItem)
@@ -54,7 +56,7 @@ while cycle:
                     print("Removed:", currentList.pop(removeIndex))
             else:
                 currentList.remove(removeItem)
-            write_current_list('todos.txt', currentList)
+            write_current_list(currentList)
             # with open('todos.txt', 'w') as todoFile:
             #     todoFile.writelines(currentList)
         case "Exit":
