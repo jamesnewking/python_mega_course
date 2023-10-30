602356f58c7c2f8fe4c84ce034bf7ff29482f4d1
jamesnewking <jamesnewking@gmail.com>
2023-10-29 19:26:08 -0700

    built GUI app to a Windows executable

--- experiments/inch_to_meter_gui.py
+++ experiments/inch_to_meter_gui.py
@@ -1,35 +1,39 @@
 import PySimpleGUI as sg
-cycle = True
+sg.theme("Black")
 feetLabel = sg.Text("Enter feet:")
 feetInput = sg.Input(key="feet")
-
 inchLabel = sg.Text("Enter inches:")
 inchInput = sg.Input(key="inches")
-convertButton = sg.Button("Convert", button_color="green")
+convertButton = sg.Button("Convert", key="convert", button_color="green")
 outputText = sg.Text("", key="output")
 meterText = sg.Text("m", key="meter")
 errorText = sg.Text("", key="error", text_color="red")
+exitButton = sg.Button("Exit", key="exit", button_color="red")
 window = sg.Window('Inch Converter',
                    layout=[[feetLabel, feetInput],
                            [inchLabel, inchInput],
-                           [convertButton, outputText, meterText],
-                           [errorText]]
+                           [convertButton, exitButton, outputText, meterText],
+                           [errorText]
+                           ]
                    )
-while cycle:
+while True:
     event, values = window.read()
-    convertFeet = values['feet']
-    convertInches = values['inches']
+    match event:
+        case "convert":
+            convertFeet = values['feet']
+            convertInches = values['inches']
+            if convertFeet.isnumeric() and convertInches.isnumeric():
+                totalInches = float(convertFeet) * 12 + float(convertInches)
+                totalMeters = totalInches / 39.37
+                window['output'].update(value=round(totalMeters,3))
+                window['error'].update(value='')
 
-    if convertFeet.isnumeric() and convertInches.isnumeric():
-        totalInches = float(convertFeet) * 12 + float(convertInches)
-        totalMeters = totalInches / 39.37
-        window['output'].update(value=round(totalMeters,3))
-        window['error'].update(value='')
-
-    else:
-        window['feet'].update(value='')
-        window['inches'].update(value='')
-        window['output'].update(value='')
-        window['error'].update(value='* Must be whole numbers *')
+            else:
+                window['feet'].update(value='')
+                window['inches'].update(value='')
+                window['output'].update(value='')
+                window['error'].update(value='* Must be whole numbers *')
+        case "exit" | sg.WIN_CLOSED:
+            break
 
 window.close()
